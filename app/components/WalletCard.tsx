ca8409e2b9df23b2abc2825437fa7114fa58def1
fixedevelloper <fixedevelloper@gmail.com>
2025-12-19 10:41:30 +0100

    prepare

--- app/components/WalletCard.tsx
+++ app/components/WalletCard.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useState } from "react";
-import { Eye, EyeOff, ArrowDownCircle, ArrowUpCircle, Receipt } from "lucide-react";
+import React, { useState } from "react";
 import { userData } from "../data/user";
 import { useSession } from "next-auth/react";
+import {ArrowDownCircle, ArrowUpCircle, Eye, EyeOff, Receipt} from "lucide-react";
 
 export default function WalletCard() {
   const [hidden, setHidden] = useState(true);
@@ -13,12 +13,43 @@ export default function WalletCard() {
   const balance = typeof session?.user?.balance === "number" ? session.user.balance : 0;
 
   return (
-    <div className="bg-white rounded-2xl shadow-md mx-4 mt-[-30px] p-4 relative z-10">
-      {/* Header */}
+
+          <div className="relative mx-4  mt-[-30px] mb-4 z-10">
+              <div
+                  className="
+          relative
+          w-full
+          overflow-hidden
+          rounded-2xl
+          shadow-lg
+          bg-black
+          h-[220px]
+          sm:h-[300px]
+          md:h-[380px]
+          lg:h-[460px]
+        "
+              >
+                  <video
+                      className="absolute inset-0 h-full w-full object-cover"
+                      src="/videos/video.mp4"
+                      poster="/logo.jpeg"
+                      autoPlay
+                      muted
+                      playsInline
+                      loop
+                      preload="none"
+                      onCanPlay={(e) => e.currentTarget.play()}
+                  />
+              </div>
+          </div>
+
+
+/*   <div className="bg-white rounded-2xl shadow-md mx-4 mt-[-30px] p-4 relative z-10">
+      {/!* Header *!/}
       <p className="text-gray-500 text-sm">Portefeuille</p>
       <p className="text-lg font-semibold text-gray-500">{name}</p>
 
-      {/* Balance */}
+      {/!* Balance *!/}
       <div className="flex items-center justify-between mt-1">
         <p className="text-xl font-bold text-gray-500">
           {hidden ? "FCFA ******" : `FCFA ${balance.toLocaleString()}`}
@@ -28,7 +59,7 @@ export default function WalletCard() {
         </button>
       </div>
 
-      {/* Actions */}
+      {/!* Actions *!/}
       <div className="flex justify-around mt-3 border-t pt-4 text-[#014d74] font-medium">
         <div className="flex flex-col items-center cursor-pointer hover:opacity-80">
           <ArrowDownCircle size={26} />
@@ -45,6 +76,6 @@ export default function WalletCard() {
           <p className="text-sm mt-1">Mes achats</p>
         </div>
       </div>
-    </div>
+    </div>*/
   );
 }

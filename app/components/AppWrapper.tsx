8dd40bce39e4e3ce5db35225caaf5eddc874233c
fixedevelloper <fixedevelloper@gmail.com>
2025-12-19 15:26:00 +0100

    prepare

--- app/components/AppWrapper.tsx
+++ app/components/AppWrapper.tsx
@@ -2,32 +2,3 @@
 import { SessionProvider } from "next-auth/react";
 import { SnackbarProvider, useSnackbar } from "notistack";
 import {ReactNode, useEffect} from "react";
-import {setupAxiosInterceptors} from "../utils/axiosServices";
-
-export type ChildrenType = Readonly<{ children: ReactNode }>
-const AxiosInterceptorProvider = () => {
-    const { enqueueSnackbar } = useSnackbar();
-
-    useEffect(() => {
-        setupAxiosInterceptors(enqueueSnackbar);
-    }, [enqueueSnackbar]);
-
-    return null;
-};
-
-const AppProvidersWrapper = ({ children }: ChildrenType) => {
-    return (
-        <SessionProvider>
-            <SnackbarProvider
-                maxSnack={3}
-                anchorOrigin={{ vertical: "top", horizontal: "right" }}
-                autoHideDuration={4000}
-            >
-                <AxiosInterceptorProvider /> {/* Avant children */}
-                {children}
-            </SnackbarProvider>
-        </SessionProvider>
-    );
-};
-
-export default AppProvidersWrapper;
